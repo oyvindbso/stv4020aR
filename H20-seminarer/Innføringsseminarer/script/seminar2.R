4fd58876d7fa8a5466a7c44ba53dd2f0936c3fed
Lise Rødland <liserodland@gmail.com>
2020-09-08 13:54:11 +0200

    Lastet opp tomt script fra seminar 2

--- H20-seminarer/Innføringsseminarer/script/seminar2.R
+++ H20-seminarer/Innføringsseminarer/script/seminar2.R
@@ -4,6 +4,8 @@
 
 # Rydder environment (dvs. gjerner alt som er i environment)
 # rm(list = ls()) # Fjern # foran om du vil kjøre denne
+# rm(aid)
+# rm(list = ls())
 
 # Til dere som sleit med å laste inn direkte fra github, prøv dette (uten #):
 # install.packages("RCurl")
@@ -11,23 +13,25 @@
 # dataurl <- getURL("https://raw.githubusercontent.com/liserodland/stv4020aR/master/H20-seminarer/Innf%C3%B8ringsseminarer/data/aid.csv")
 # aid <- read.csv(text = dataurl,
 #                 stringsAsFactors = FALSE)
-
-
+# rm(dataurl)
 
 # litt om set.wd()
+# litt om ?pakkenavn
 
 # Laster inn pakker 
 library(tidyverse)
+#install.packages("haven")
 library(haven)
 
 # Laster inn data
-aid <-  read_dta("../../data/aid.dta")
+aid <-  read_dta("H20-seminarer/Innføringsseminarer/data/aid.dta")
+#read_sav
 
 # Undersøker datasettet
 str(aid) 
 aid 
-head(aid, 10) 
-tail(aid, 10) 
+head(aid, 2) 
+tail(aid, 4) 
 sample_n(aid, 10)
 
 # Finner navn på variabler
@@ -40,27 +44,20 @@ table(is.na())
 # Oppretter alternativ policy-variabel
 # Base R
 
-
 # Tidyverse (vanlig + sentrert)
-
 
 # Omkoder klasser (country + gdp_growth)
 
-
 # Ifelse med periode for å lage tiårsvariabel
-
 
 ## Aggregering
 
 
 # Oppretter region-variabel
 
-
 # Group by region og summarise
 
-
 # Group by region og mutate
-
 
 # Bivariat korrelasjon
 
