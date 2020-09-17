835c040634cde0c92fbbf5d525d653bd7987498c
Lise Rødland <liserodland@gmail.com>
2020-09-17 16:06:59 +0200

    oppdatert script faktoranalyse

--- H20-seminarer/Fordypningsseminarer/script/faktoranalyse.R
+++ H20-seminarer/Fordypningsseminarer/script/faktoranalyse.R
@@ -25,20 +25,23 @@ str(ess_no)
 # TRINN 1: Forberede dataene
 # Deskriptiv statistikk
 ess_no %>%
+  select(starts_with("trust")) %>% 
   summary()
 # Hvilket målenivå er variablene på? Vi vil helst ha metriske og forholdstallsnivå
 # Bør ha sju verdier (Christophersen s 97)
 # Går variablene i samme retning?  Sjekk kodebok og evnt. labels 
 summary(ess_no$trust_police)
 get_labels(ess_no$trust_police)
+get_labels(ess_no$trust_legal)
 
 # Har vi mye missing? Hva skal vi gjøre med det?
-table(complete.cases(ess_no))
+table(complete.cases(ess_no %>% 
+                       select(starts_with("trust"))))
 # Ser at 387 observasjoner har missing på en eller flere av
 # variablene våre. Dette kan være lurt å diskutere i hjemmeoppgaven,
 # spesielt dersom du velger å fjerne de med na ved hjelp av na.omit()
 
-## Ser på korrelasjoner mellom tillits- og tilfredshetsvariabler
+## Ser på korrelasjoner mellom tillitsvariabler
 # Korrelasjonsmatrise (Pearsons R)
 korrel <- ess_no %>%
   select(starts_with("trust")) %>%
@@ -54,7 +57,7 @@ cor.plot(korrel, numbers = TRUE)
 
 # Alternativ metode ved hjelp av ggcorrplot som tar utgangspunkt
 # i ggplot  fra pakken ggcorrplot
-ggcorrplot(korrel, show.diag = FALSE, lab = TRUE,
+ggcorrplot(korrel, show.diag = TRUE, lab = TRUE,
            hc.order =  TRUE, type = "upper",
            ggtheme = theme_void())
 
@@ -82,12 +85,12 @@ bart_spher(korrel)
 trust_prin <- princomp(~.,    # Her skal vi ha en formel uten AVAR. Med "." sier vi at vi vil ta med alt i datasettet
                        ess_no %>%
                          select(starts_with("trust")),
-                       scores = TRUE, na.action = "na.omit")
+                       scores = TRUE, na.action = "na.exclude")
 
 loadings(trust_prin)
 
 ### Hvor mange faktorer skal vi ha?
-## Kaisers kriterium: : De faktorene som har eigenvalue større
+## Kaisers kriterium: De faktorene som har eigenvalue større
 # enn 1, skal tas med
 # Eigenvalues henger sammen med hvor stor andel forklart varians en faktor står for
 # Legger til grunn standardiserte variabler som da vil ha total varians
@@ -98,7 +101,7 @@ eigen
 # Summen av alle eigen-verdiene er lik antall indikatorer:
 sum(eigen$values)
 
-# Kaises kriterium: Hver faktor bør forklare minst like mye som en enkeltstående indikator (>=1). 
+# Kaisers kriterium: Hver faktor bør forklare minst like mye som en enkeltstående indikator (>=1). 
 eigen$values
 #.. egentlig bare 1? 
 
@@ -132,7 +135,7 @@ loadings(trust_prin) # Hva forteller denne oss?
 # Vi kan ha en ganske tydelig, men uhøytidelig, teoretisk antagelse om hvorfor 
 # våre variabler deler seg i tre faktorer: 
 ## en faktor handler om nasjonal politikk (parlament, politiske partier, politikere) 
-## en handler om rettsvesen (rettssystem,politi)
+## en handler om rettsvesen (rettssystem, politi)
 ## en handler om internasjonale institusjoner (EU, FN).
 
 
@@ -146,7 +149,6 @@ print(loadings(trust_factor1), cutoff = .4)
 trust_factor2 <- factanal(~., 2, ess_no %>%
                                     select(starts_with("trust")))
 
-
 names(trust_factor2)
 print(loadings(trust_factor2), cutoff = .4)
 # Faktorladningene kan tolkes som standardiserte regresjonskoeffisienter
@@ -156,7 +158,7 @@ print(loadings(trust_factor2), cutoff = .4)
 trust_factor3 <- factanal(~., 3, ess_no %>%
                             select(starts_with("trust")))
 
-print(loadings(trust_factor3), cutoff = .5)
+print(loadings(trust_factor3), cutoff = .4)
  
 ## ROTASJON
 # For å få "renere" faktorer
@@ -181,9 +183,9 @@ promax(loadings(trust_factor3))
 ?quartimax
 
 ## Opprette additive indekser - eksempel (her finnes det mange muligheter - se pensum/forelesning):
-ess$political_trust <- (ess$trust_parl + ess$trust_politicians + ess$trust_polparties) / 3
-ess$legal_trust <- (ess$trust_legalsys + ess$trust_police) / 2
-ess$international_trust <- (ess$trust_unitednations + ess$trust_eurparl) / 2
+ess_no$political_trust <- (ess_no$trust_parliament + ess_no$trust_politicians + ess_no$trust_polparties) / 3
+ess_no$legal_trust <- (ess_no$trust_legal + ess_no$trust_police) / 2
+ess_no$international_trust <- (ess_no$trust_un + ess_no$trust_ep) / 2
 
 
 ## Opprette faktorindekser
@@ -204,7 +206,7 @@ trust_factor3score$scores
 # Husk na.action = "na.exclude" i modellen over.
 ess_no$politicaltrust_score <- trust_factor3score$scores[, 1]
 ess_no$legaltrust_score <- trust_factor3score$scores[, 2]
-ess_no$inttrust_score <- trust_factor3score$scores[, 2]
+ess_no$inttrust_score <- trust_factor3score$scores[, 3]
 
 summary(ess_no$politicaltrust_score)
 summary(ess_no$legaltrust_score)
@@ -220,5 +222,4 @@ trust_prin_alt <- PCA(ess_no%>%
 # https://learn.datacamp.com/courses/dimensionality-reduction-in-r
 # PCA graph of variables: 
 # Her ser vi at dimensjon nr 2 skiller de som har høy verdi på
-# satisfied fra de med lav verdi på tillit 
-
+# satisfied fra de med lav verdi på tillit
